e0d87ce6c51df9ff2e0476ef90b1c390b0dc1b6d
najahn <najko.jahn@gmail.com>
2022-03-10 19:19:55 +0100

    Replace NAs with 0

--- data-raw/002_get_cr_data.R
+++ data-raw/002_get_cr_data.R
@@ -20,7 +20,8 @@ create_bq_table <- function(sql_basename = NULL,
     bigrquery::bq_table_delete(dest)
 
   # Read sql query
-  sql_string <- system.file(package = "hoaddata", paste0("inst/sql/", sql_basename, ".sql"))
+  sql_string <-
+    system.file(package = "hoaddata", paste0("inst/sql/", sql_basename, ".sql"))
   if (sql_string == "")
     stop(paste(sql, "file not found"))
   sql_ <-  readr::read_file(sql_string)
@@ -44,28 +45,44 @@ create_bq_table(sql_basename = "cc_md")
 ### Creative Commons per journals ----
 cc_jn_ind <- create_bq_table("cc_jn_ind", download = TRUE) |>
   dplyr::mutate(cr_year = factor(cr_year))  |>
-  dplyr::mutate(cc = factor(cc,
-                               # Order by permissiveness
-                               levels = c("CC BY", "CC BY-SA",
-                                          "CC BY-NC", "CC BY-NC-SA",
-                                          "CC BY-ND", "CC BY-NC-ND")))
+  dplyr::mutate(cc = factor(
+    cc,
+    # Order by permissiveness
+    levels = c(
+      "CC BY",
+      "CC BY-SA",
+      "CC BY-NC",
+      "CC BY-NC-SA",
+      "CC BY-ND",
+      "CC BY-NC-ND"
+    )
+  )) |>
+  mutate(across(c(cc_total, prop), ~ tidyr::replace_na(., 0)))
+
 usethis::use_data(cc_jn_ind, overwrite = TRUE)
 
 ## Affiliations (OpenAlex)  ----
 
 ### Metadata ----
 
+# Article-level first author affiliation data, including dois
+# where no affiliation data could be found
 create_bq_table("cr_openalex_inst_full")
+
+### First-author affiliation data CC articles ----
+
+# Article-level first author affiliation data CC licenses
+cc_openalex_inst <-
+  create_bq_table("cc_openalex_inst", download = TRUE)
+usethis::use_data(cc_openalex_inst, overwrite = TRUE)
 
 ### Aggregated first-author country affiliations per hybrid journal and year ----
 
-cc_openalex_inst_jn_ind <- create_bq_table("cc_openalex_inst_jn_ind",
-                                           download = TRUE)
+cc_openalex_inst_jn_ind_tmp <-
+  create_bq_table("cc_openalex_inst_jn_ind",
+                  download = TRUE)
+
+cc_openalex_inst_jn_ind <- cc_openalex_inst_jn_ind_tmp |>
+  mutate(across(c(cc_articles, prop), ~ tidyr::replace_na(., 0)))
+
 usethis::use_data(cc_openalex_inst_jn_ind, overwrite = TRUE)
-
-
-### First-author affiliation data CC articles ----
-cc_openalex_inst <- create_bq_table("cc_openalex_inst", download = TRUE)
-usethis::use_data(cc_openalex_inst, overwrite = TRUE)
-
-
--- inst/sql/cr_openalex_inst_full.sql
+++ inst/sql/cr_openalex_inst_full.sql
@@ -1,29 +1,32 @@
+WITH
+  first_aff AS (
+  SELECT
+    doi,
+    author_position,
+    id,
+    country_code,
+    display_name
+  FROM (
+    SELECT
+      doi,
+      author_position,
+      institutions
+    FROM
+      `subugoe-collaborative.openalex.works`,
+      UNNEST(authorships)
+    WHERE
+      author_position = "first" ),
+    UNNEST(institutions) )
 SELECT
-  doi,
+  cr_raw.doi,
   issn_l,
   cr_year,
   id,
   country_code,
   display_name
-FROM (
-  SELECT
-    doi,
-    issn_l,
-    cr_year,
-    institutions
-  FROM (
-    SELECT
-      cc_md.doi AS doi,
-      issn_l,
-      cr_year,
-      authorships
-    FROM
-      `hoad-dash.oam.cc_md` AS cc_md
-    LEFT JOIN
-      `subugoe-collaborative.openalex.works` AS openalex
-    ON
-      cc_md.doi = openalex.doi ),
-    UNNEST(authorships)
-  WHERE
-    author_position = "first"),
-  UNNEST(institutions)
+FROM
+  `hoad-dash.oam.cr_raw` AS cr_raw
+LEFT OUTER JOIN
+  first_aff
+ON
+  cr_raw.doi = first_aff.doi
