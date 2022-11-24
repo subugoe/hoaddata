64169a56376dddcb1f0262b72e51bb5fc3a1d89f
najahn <najko.jahn@gmail.com>
2022-11-24 18:38:08 +0100

    De-duplicate issn-l

--- tests/testthat/test-data-jn_aff.R
+++ tests/testthat/test-data-jn_aff.R
@@ -20,26 +20,26 @@ testthat::test_that("cc_articles format is correct", {
   testthat::expect_gt(nrow(cc_articles), 350000)
 })
 
-# testthat::test_that("cc_articles no duplicated journal ids", {
-#  tmp <- cc_articles %>%
-#    dplyr::distinct(doi, issn_l) %>%
-#    dplyr::group_by(doi) %>%
-#    dplyr::filter(dplyr::n() > 1)
-#  testthat::expect_equal(nrow(tmp), 0)
-#})
+ testthat::test_that("cc_articles no duplicated journal ids", {
+  tmp <- cc_articles %>%
+    dplyr::distinct(doi, issn_l) %>%
+    dplyr::group_by(doi) %>%
+    dplyr::filter(dplyr::n() > 1)
+  testthat::expect_equal(nrow(tmp), 0)
+})
 
-# testthat::test_that("cc_articles cc breakdown equal to cc_jn_df", {
-#   cc_1 <- jn_ind %>%
-#     dplyr::filter(!is.na(cc)) %>%
-#     # To Do: Should cc be factored according to level of permissiveness
-#     # throughout of hoaddata?
-#     dplyr::group_by(cc = as.character(cc)) %>%
-#     dplyr::summarise(articles = sum(cc_total)) %>%
-#     dplyr::arrange(dplyr::desc(articles))
-#   cc_2 <- cc_articles %>%
-#     dplyr::group_by(cc) %>%
-#     dplyr::summarise(articles = dplyr::n_distinct(doi)) %>%
-#     dplyr::arrange(dplyr::desc(articles))
+ testthat::test_that("cc_articles cc breakdown equal to cc_jn_df", {
+   cc_1 <- jn_ind %>%
+     dplyr::filter(!is.na(cc)) %>%
+     # To Do: Should cc be factored according to level of permissiveness
+     # throughout of hoaddata?
+     dplyr::group_by(cc = as.character(cc)) %>%
+     dplyr::summarise(articles = sum(cc_total)) %>%
+     dplyr::arrange(dplyr::desc(articles))
+   cc_2 <- cc_articles %>%
+     dplyr::group_by(cc) %>%
+     dplyr::summarise(articles = dplyr::n_distinct(doi)) %>%
+     dplyr::arrange(dplyr::desc(articles))
 
-#   testthat::expect_equal(cc_1, cc_2)
-# })
+   testthat::expect_equal(cc_1, cc_2)
+ })
