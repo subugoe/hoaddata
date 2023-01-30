b6385f9635d505f323e366014a9923c979ce3a4f
najahn <najko.jahn@gmail.com>
2023-01-30 11:30:39 +0100

    Remove csv export

--- data-raw/jct_001_get_cr_data.R
+++ data-raw/jct_001_get_cr_data.R
@@ -2,11 +2,11 @@
 pkgload::load_all()
 
 #' Authentificate with Big Query
-#' 
+#'
 #'Decrypting-ability using internal secret_* functions from gargle
 # package following bigrquery test setup.
 # <https://gargle.r-lib.org/articles/articles/managing-tokens-securely.html>
-#' 
+#'
 if (gargle:::secret_can_decrypt(package = "hoaddata")) {
   gc_json <-
     gargle:::secret_read(package = "hoaddata", name = "hoaddata-bq.json")
@@ -131,8 +131,7 @@ jn_ind <- create_bq_table("cc_jn_ind", download = TRUE) |>
 
 # Export as package data
 usethis::use_data(jn_ind, overwrite = TRUE)
-# Save as csv
-readr::write_csv(jn_ind, "data-raw/jn_ind.csv")
+
 
 ## Affiliations (OpenAlex)  ----
 
@@ -167,8 +166,7 @@ cc_articles <-
   create_bq_table("cc_openalex_inst", download = TRUE)
 # Save in package
 usethis::use_data(cc_articles, overwrite = TRUE)
-# Save as csv
-readr::write_csv(cc_articles, "data-raw/cc_articles.csv")
+
 
 ### Aggregated first-author country affiliations per hybrid journal and year ----
 
@@ -178,8 +176,6 @@ jn_aff <-
                   download = TRUE)
 # Save in package
 usethis::use_data(jn_aff, overwrite = TRUE)
-# Save as csv
-readr::write_csv(jn_aff, "data-raw/jn_aff.csv")
 
 ### Open Metadata ----
 
@@ -194,13 +190,12 @@ upw_cr_de <- create_bq_table("cc_upw_cr_de", download = TRUE) |>
 cr_upw <- dplyr::bind_rows(upw_cr, upw_cr_de)
 # Save in package
 usethis::use_data(cr_upw, overwrite = TRUE)
-# Save as csv
-readr::write_csv(cr_upw, "data-raw/cr_upw.csv")
 
-### Crossref metadata coverage CC-licenses articles 
+
+### Crossref metadata coverage CC-licenses articles
 
 #### Global
-cc_md_indicators <- create_bq_table("cc_md_indicators", download = TRUE) 
+cc_md_indicators <- create_bq_table("cc_md_indicators", download = TRUE)
 
 #### Germany
 cc_md_indicators_de <- create_bq_table("cc_md_indicators_de", download = TRUE)
@@ -212,22 +207,20 @@ cr_md <- cc_md_indicators |>
   dplyr::arrange(issn_l, cr_year)
 # Save in package
 usethis::use_data(cr_md, overwrite = TRUE)
-# Save as csv
-readr::write_csv(cr_md, "data-raw/cr_md.csv")
+
 
 ### OpenAlex Journal metadata ----
 jct_oalex_venues <- create_bq_table("jct_oalex_venues", download = TRUE)
 # Save in package
 usethis::use_data(jct_oalex_venues, overwrite = TRUE)
 
-readr::write_csv(jct_oalex_venues, "data-raw/jct_oalex_venues.csv")
 
 ### Link country affiliations and TAs ----
 jct_inst <- readr::read_csv("data-raw/jct_institutions.csv")
 jct_inst_short <- jct_inst |>
   dplyr::distinct(esac_id, ror_id) |>
-  # Remove orgs without ror id 
-  dplyr::filter(!is.na(ror_id)) 
+  # Remove orgs without ror id
+  dplyr::filter(!is.na(ror_id))
 
 # Upload to BQ
 jct_inst_short_path <-
@@ -247,5 +240,3 @@ ta_country_output <-
   create_bq_table("ta_country_output", download = TRUE)
 # Save in package
 usethis::use_data(ta_country_output, overwrite = TRUE)
-# Save as csv
-readr::write_csv(ta_country_output, "data-raw/jct_ta_country_output.csv")
