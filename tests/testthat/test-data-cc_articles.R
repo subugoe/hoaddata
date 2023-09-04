83d49458840d4784d3b6fad39629bb8a3a9a7385
najahn <najko.jahn@gmail.com>
2023-09-04 11:44:47 +0200

    Fix cc license for both am and vor

---
+++ tests/testthat/test-data-cc_articles.R
@@ -0,0 +1,16 @@
+# Test Hybrid OA articles
+testthat::test_that("cc_articles exists", {
+  testthat::expect_s3_class(cc_articles, "tbl_df")
+})
+
+testthat::test_that("cc_articles has OA article", {
+  testthat::expect_equal(nrow
+  (cc_articles[cc_articles$doi == "10.1002/2016jc012241", ]),
+  1)
+})
+
+testthat::test_that("cc_articles has OA article", {
+  testthat::expect_equal(nrow
+  (cc_articles[cc_articles$doi == "10.1002/asi.24460", ]),
+  1)
+})
