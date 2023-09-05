8389f9e3ef161e78ea81df266d61e9913ba11be1
najahn <najko.jahn@gmail.com>
2023-09-05 11:01:56 +0200

    Reduce package size

--- inst/sql/cc_openalex_inst.sql
+++ inst/sql/cc_openalex_inst.sql
@@ -2,11 +2,11 @@ SELECT DISTINCT cc_md.doi,
   cc_md.issn_l,
   cc_md.cr_year,
   cc,
-  country_code,
-  id,
-  display_name
+  inst.country_code,
+  ror
 FROM `subugoe-collaborative.hoaddata.cc_md` AS cc_md
   LEFT JOIN `subugoe-collaborative.hoaddata.cr_openalex_inst_full` as inst ON cc_md.doi = inst.doi
+  LEFT JOIN `subugoe-collaborative.openalex.institutions` as oalex ON inst.id = oalex.id
 WHERE vor = 1
   AND NOT cc IS NULL # there are few cases where the cc regex extraction did not work
 ORDER BY doi
